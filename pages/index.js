bae8aa5d2f4233563555d8cd5a4e2800ae7df6cc
timefliez1210 <crfabig@gmail.com>
2020-07-11 18:06:44 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,192 +1,232 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>
+          How to earn money online - The new world covid-19 forces us into
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <body>
+        <section className="header">
+          <div className="content-holder">
+            <h1>
+              You are spending hours a day online?
+              <br />
+              <br />
+              But still don't earn money? <br />
+              You are doing it wrong.
+            </h1>
+            <a className="button" href="#content">
+              Why?
+            </a>
+          </div>
+        </section>
+        <section className="content" id="content">
+          <div className="wrapper">
+            <h1>Let's talk about now and the future</h1>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <div className="card">
+              <div>
+                <img width="80%" src="./assets/img/covid-19_corona-virus.jpg" />
+              </div>
+              <div>
+                <h2>
+                  The year 2020, covid-19 and how they (should) change our view
+                  of the world.
+                </h2>
+                <p>
+                  Last year was a normal year, most people had a permanent job,
+                  worked “9-to-5”, came home, paid the bills and were happy if
+                  something was left over. Many dreamed of an independent life,
+                  independent of location and in a modern way, but never put it
+                  into practice.
+                </p>
+                <h2>But what is happening at the moment?</h2>
+                <p>
+                  The current situation shows us that the system that we are
+                  taught from an early age has its limits, we are forced to
+                  either endure and persevere or finally to exhaust the
+                  possibilities that our time gives us, believe it or not, it
+                  are more than enough people who still make their money the
+                  easiest way regardless of covid-19.
+                </p>
+              </div>
+            </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
+            <h2>“That’s all very nice, but how do I do this?”</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Well, I won't lie here. It’s a long way ahead of you which will
+              take a lot of dedication and discipline, sadly there is no
+              “you-will-get-rich-overnight”-scheme, so plan in your time to
+              build things up, but what I can tell you: It’s worth it. I am a 30
+              years old german, earning exclusively online, and were able to
+              move to the philippines. So if you want to live independently, or
+              simply just a little money from the side, drop your email here and
+              I can show you step-by-step how you can achieve this too.
             </p>
-          </a>
-        </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+            <div className="card">
+              <div className="display-image2">
+                <img src="./assets/img/ethereum.jpg" width="80%" />
+              </div>
+              <div>
+                <h2>How to start:</h2>
+                <p>
+                  Last year was a normal year, most people had a permanent job,
+                  worked “9-to-5”, came home, paid the bills and were happy if
+                  something was left over. Many dreamed of an independent life,
+                  independent of location and in a modern way, but never put it
+                  into practice.
+                </p>
+                <h2>The exciting new world</h2>
+                <p>
+                  The current situation shows us that the system that we are
+                  taught from an early age has its limits, we are forced to
+                  either endure and persevere or finally to exhaust the
+                  possibilities that our time gives us, believe it or not, it
+                  are more than enough people who still make their money the
+                  easiest way regardless of covid-19.
+                </p>
+              </div>
+            </div>
+            <h1>
+              The Blockchain, Ethereum and how this exiting new world can help
+            </h1>
+            <div className="card">
+              <div className="display-image2">
+                <img src="./assets/img/blockchain.jpg" width="80%" />
+              </div>
+              <div>
+                <h2>So what is this Blockchain and how it can help me?</h2>
+                <p>
+                  I will try to keep it simple, you can imagine a Blockchain as
+                  a decentralized database. But the difference between a
+                  classical database is that data, once put into a Blockchain,
+                  can NOT be changed anymore. We call this immutable.
+                  <br />
+                  <br />
+                  This way of handling data, transactions or apps builds a
+                  trustless environment which is governed by a smart contract
+                  which can never be changed or disappear. And everybody who
+                  wants to deal with a smart contract can read it first, before
+                  any interaction because it's public. So if you use these
+                  contracts to start a business model, it doesn’t matter what
+                  any government says about it, they can not close it or
+                  interfere in it, hackers can not steal your data and personal
+                  information AND you can read the contract in the very start to
+                  verify that nobody messes with you. Isn't this already awesome
+                  by itself? Being totally independent?
+                  <br />
+                  <br />
+                  <b>
+                    But how can this help you earn money? Just go ahead, it’s
+                    simple!
+                  </b>
+                  <br />
+                  <br /> <br />
+                  <br />
+                </p>
+                {/* <Link to="voomo"> */}
+                <a className="button">One Smartcontract to rule them all</a>
+                {/* </Link> */}
+              </div>
+            </div>
+          </div>
+        </section>
+      </body>
 
       <style jsx>{`
-        .container {
+        .header {
           min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          align-content: center center;
+
+          background-image: url("assets/img/blockchain-banner.jpg");
+          background-color: #cccccc;
+          background-position: bottom left;
+          background-repeat: no-repeat;
+          background-size: cover;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
+        .content {
+          min-height: 100vh;
           display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          align-content: center center;
+          padding: 100px 15px;
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+        .content-holder {
+          width: 95vw;
+          max-width: 1080px;
+          margin: auto auto;
+          color: white;
+          background: rgba(0, 0, 0, 0.7);
+          border-radius: 10px;
+          box-shadow: 10px 10px 20px #000000, -10px -10px 20px #000000;
           text-align: center;
+          padding: 50px 5px;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
+        .wrapper {
+          width: 95vw;
+          max-width: 1080px;
+          margin: 0 auto;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        h1 {
+          text-align: center;
+          font-size: 32px;
         }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+        h2,
+        h3 {
+          text-align: left;
+        }
 
-          max-width: 800px;
-          margin-top: 3rem;
+        p {
+          text-align: left;
+          line-height: 18px;
+        }
+
+        .button {
+          background-color: black;
+          color: white;
+          padding: 20px 30px;
+          border-radius: 15px;
+          width: 300px;
+          margin: auto auto;
+          font-style: bold;
+          text-decoration: none;
+          font-size: 20px;
         }
 
         .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+          margin: 5vh auto;
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+          padding: 20px;
+          border-radius: 40px;
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+          position: relative;
+          z-index: 0;
         }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
+        .display-imgage {
+          height: 100%;
+          align-self: center;
+          z-index: 0;
+          border-radius: 40px;
+          box-shadow: 5px 5px 10px #000910, -5px -5px 10px #002340;
         }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        .display-imgage2 {
+          width: 100px;
+          align-self: center;
+          z-index: 0;
+          border-radius: 40px;
+          box-shadow: 5px 5px 10px #000910, -5px -5px 10px #002340;
         }
       `}</style>
 
@@ -204,6 +244,80 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
-  )
+    </>
+  );
+}
+
+{
+  /* <div id="mc_embed_signup">
+              <form
+                action="https://gmail.us10.list-manage.com/subscribe/post?u=30daa631debe9c059299c8e7a&amp;id=bb6e405363"
+                method="post"
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                class="validate"
+                target="_blank"
+                novalidate
+              >
+                <div id="mc_embed_signup_scroll">
+                  <h2>Subscribe</h2>
+                  <div class="indicates-required">
+                    <span class="asterisk">*</span> indicates required
+                  </div>
+                  <div class="mc-field-group">
+                    <label for="mce-EMAIL">
+                      Email Address <span class="asterisk">*</span>
+                    </label>
+                    <input
+                      type="email"
+                      value=""
+                      name="EMAIL"
+                      class="required email"
+                      id="mce-EMAIL"
+                    />
+                  </div>
+                  <div class="mc-field-group">
+                    <label for="mce-FNAME">First Name </label>
+                    <input
+                      type="text"
+                      value=""
+                      name="FNAME"
+                      class=""
+                      id="mce-FNAME"
+                    />
+                  </div>
+                  <div class="mc-field-group">
+                    <label for="mce-LNAME">Last Name </label>
+                    <input
+                      type="text"
+                      value=""
+                      name="LNAME"
+                      class=""
+                      id="mce-LNAME"
+                    />
+                  </div>
+                  <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response"></div>
+                    <div class="response" id="mce-success-response"></div>
+                  </div>
+                  <div aria-hidden="true">
+                    <input
+                      type="text"
+                      name="b_30daa631debe9c059299c8e7a_bb6e405363"
+                      tabindex="-1"
+                      value=""
+                    />
+                  </div>
+                  <div class="clear">
+                    <input
+                      type="submit"
+                      value="Subscribe"
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                      class="button"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div> */
 }
---
+++ pages/voomo.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+const Voomo = () => {
+  return (
+    <div>
+      <h1>Hello from Voomo</h1>
+    </div>
+  );
+};
+
+export default Voomo;
